892ee763c4e12a79ebd18ce5185e6e0cc67707f6
Brock Vandenbergh <cheesecakebird2014@gmail.com>
2024-08-08 20:07:49 +1000

    deleted unused views and other things

--- src/main/java/com/gradeapp/util/GradeCalculator.java
+++
@@ -1,9 +0,0 @@
-package com.gradeapp.util;
-
-import com.gradeapp.model.Course;
-import com.gradeapp.model.Student;
-
-public interface GradeCalculator {
-    double calculateFinalGrade(Student student, Course course);
-}
-
